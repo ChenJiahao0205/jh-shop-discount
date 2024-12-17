3c63071d05f1b9640bb61b558cf495baa63df310
chenjiahao <834713285@qq.com>
2024-12-17 22:23:45 +0800

    feat：调试feign接口

--- jh-shop-discount-api/src/main/java/pers/jhshop/fapi/discount/service/fallback/TestApiServiceFallbackFactory.java
+++ jh-shop-discount-api/src/main/java/pers/jhshop/fapi/discount/service/fallback/TestApiServiceFallbackFactory.java
@@ -2,16 +2,17 @@ package pers.jhshop.fapi.discount.service.fallback;
 
 import org.springframework.cloud.openfeign.FallbackFactory;
 import org.springframework.stereotype.Component;
+import pers.jhshop.fapi.discount.service.TestApiService;
 
 /**
  * @author ChenJiahao(五条)
  * @date 2024/12/10 21:56:51
  */
 @Component
-public class TestApiServiceFallbackFactory implements FallbackFactory<TestApiServiceFallback> {
+public class TestApiServiceFallbackFactory implements FallbackFactory<TestApiService> {
 
     @Override
-    public TestApiServiceFallback create(Throwable cause) {
+    public TestApiService create(Throwable cause) {
         return new TestApiServiceFallback(cause);
     }
 }
